2e80448e5f49dfd8199af7a60315754871ff2c9c
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-01-27 20:50:00 -0600

    dynamically create card elements using data pulled from API fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ const tempSpan = document.getElementById("temp");
 const windSpan = document.getElementById("wind");
 const humidSpan = document.getElementById("humidity");
 const uvSpan = document.getElementById("uv-index");
+const forecastHeader = document.getElementById('forecast-header');
+const cardRow = document.getElementById('card-row');
 
 var formHandler = function (event) {
   event.preventDefault();
@@ -51,13 +53,14 @@ var displayWeather = function (city, searchTerm) {
     city.coord.lat +
     "&lon=" +
     city.coord.lon +
-    "&exclude=hourly&appid=292188850e48297da1a005edf38e34bf";
+    "&exclude=hourly&units=imperial&appid=292188850e48297da1a005edf38e34bf";
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
         uvSpan.textContent = data.current.uvi;
+        console.log(data);
+        displayFiveDay(data);
       });
     }
   });
@@ -86,5 +89,46 @@ var saveSearchTerm = function (searchTerm) {
   listItem.textContent = searchTerm;
   cityList.appendChild(listItem);
 };
+
+var displayFiveDay = function(forecast) {
+  forecastHeader.classList += 'visible';
+  cardRow.classList += 'visible';
+  for (let i = 1; i < 6; i++) {
+    var card = document.createElement('div');
+    card.classList = 'card border col-12 col-lg-2 p-0 mb-5';
+
+    var cardHeader = document.createElement('div');
+    cardHeader.className = 'card-header';
+    var h5 = document.createElement('div');
+    h5.className = 'h5';
+    h5.textContent = '1/26/2022';
+    cardHeader.appendChild(h5)
+    var icon = document.createElement('h4');
+    icon.textContent = '☁️';
+    cardHeader.appendChild(icon);
+    card.appendChild(cardHeader);
+
+    var listEl = document.createElement('ul');
+    listEl.className = 'list-group';
+    var tempEl = document.createElement('li');
+    tempEl.className = 'list-group-item';
+    tempEl.textContent = 'Temp: ' + forecast.daily[i].temp.day + ' °F';
+    var windEl = document.createElement('li');
+    windEl.className = 'list-group-item';
+    windEl.textContent = 'Wind: ' + forecast.daily[i].wind_speed + ' MPH';
+    var humidEl = document.createElement('li');
+    humidEl.className = 'list-group-item';
+    humidEl.textContent = 'Humidity: ' + forecast.daily[i].humidity + ' %';
+
+    listEl.appendChild(tempEl);
+    listEl.appendChild(windEl);
+    listEl.appendChild(humidEl);
+
+    card.appendChild(listEl);
+
+    cardRow.appendChild(card);
+  }
+  console.log(forecast);
+}
 
 searchForm.addEventListener("submit", formHandler);
